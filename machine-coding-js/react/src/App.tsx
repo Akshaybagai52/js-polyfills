52cb4dcacbfe153a75f63c219e15b6d1ff1c3175
Akshaybagai52 <akshaybagai52@gmail.com>
2025-09-12 01:01:42 +0530

    feat: rearrange files and added react setup

---
+++ machine-coding-js/react/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Pagination from "./challanges/InfiniteScroll/InfiniteScroll";
+
+function App() {
+  return (
+    <main>
+      <Pagination />
+    </main>
+  );
+}
+
+export default App;
---
+++ machine-coding-js/react/src/challanges/InfiniteScroll/InfiniteScroll.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InfiniteScroll = () => {
+  return (
+    <div className=''>InfiniteScroll</div>
+  )
+}
+
+export default InfiniteScroll
